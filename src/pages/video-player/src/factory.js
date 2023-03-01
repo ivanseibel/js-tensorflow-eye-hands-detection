292a7716e3bb527a59e174988595bf3076b11fad
Ivan Seibel <me@ivanseibel.dev>
2023-03-01 18:30:53 +0000

    initialize camera

--- src/pages/video-player/src/factory.js
+++ src/pages/video-player/src/factory.js
@@ -1,8 +1,10 @@
 import Controller from "./controller.js"
 import Service from "./service.js"
 import View from "./view.js"
+import Camera from "../../../lib/shared/camera.js"
 
 const [rootPath] = window.location.href.split('/pages/')
+const camera = await Camera.init()
 
 const factory = {
   async initialize() {
