e2bf96e62aa593139b1570049e5ff847d608d616
Ivan Seibel <me@ivanseibel.dev>
2023-03-03 19:41:04 +0000

    worker imports for firefox

--- src/pages/video-player/src/factory.js
+++ src/pages/video-player/src/factory.js
@@ -6,18 +6,39 @@ import { supportsWorkerType } from "../../../lib/shared/util.js"
 
 async function getWorker() {
   if (supportsWorkerType()) {
-    console.log('Web worker supported')
+    console.log('Initializing ESM workers')
     const worker = new Worker('./src/worker.js', { type: 'module' })
-    return worker;
+
+    return worker
   }
 
-  const worckerMock = {
-    postMessage: () => { },
-    onmessage: () => { },
-  }
+  console.warn(`Your browser doesn't support ESM modules on webworkers!`)
+  console.warn(`Importing libraries...`)
+  await import("https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js")
+  await import("https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js")
 
-  console.log('Web worker not supported')
-  return worckerMock
+  console.warn(`Using worker mock instead!`)
+  const service = new Service({
+    faceLandmarksDetection: window.faceLandmarksDetection
+  })
+
+  const workerMock = {
+    async postMessage(video) {
+      const blinked = await service.handBlinked(video)
+      if (!blinked) return;
+      workerMock.onmessage({ data: { blinked } })
+    },
+    // It will be overwritten by the controller
+    onmessage(msg) { }
+  }
+  console.log('Loading tf model...')
+  await service.loadModel()
+  console.log('tf model loaded!')
+
+  setTimeout(() => worker.onmessage({ data: 'READY' }), 500);
+  return workerMock
 }
 
 const worker = await getWorker()
@@ -29,7 +50,8 @@ const factory = {
   async initialize() {
     return Controller.initialize({
       view: new View(),
-      service: new Service({}),
+      worker,
+      camera,
     })
   },
 }
