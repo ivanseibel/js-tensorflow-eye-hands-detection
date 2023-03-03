4672b87c489ac97adc0eb281b83b0506245fd67d
Ivan Seibel <me@ivanseibel.dev>
2023-03-03 19:40:30 +0000

    get camera captures and processes blinks

--- src/pages/video-player/src/controller.js
+++ src/pages/video-player/src/controller.js
@@ -1,10 +1,13 @@
 export default class Controller {
   #view
-  #service
+  #worker
+  #blinkCounter = 0
+  #camera
 
-  constructor({ view, service }) {
+  constructor({ view, worker, camera }) {
     this.#view = view
-    this.#service = service
+    this.#worker = this.#configureWorker(worker)
+    this.#camera = camera
 
     this.#view.configureOnBtnClick(this.onBtnClick.bind(this))
   }
@@ -19,11 +22,52 @@ export default class Controller {
     console.log('Controller initialized')
   }
 
+  #configureWorker(worker) {
+    let ready = false
+    worker.onmessage = (msg) => {
+      if (msg.data === 'READY') {
+        console.log('Worker ready')
+        this.updateStatus('Ready to process images')
+        this.#view.enableButton()
+        ready = true
+        return;
+      }
+
+      const blinked = msg.data
+      this.#blinkCounter += 1
+      this.#view.togglePlayPause()
+      console.log('blinked', blinked)
+    }
+
+    return {
+      send(msg) {
+        if (!ready) return
+
+        worker.postMessage(msg)
+      }
+    }
+  }
+
   updateStatus(text) {
-    this.#view.updateStatus(`logger: ${text}`)
+    const times = `     - blinked ${this.#blinkCounter} times`
+    this.#view.updateStatus(`logger: ${text}`.concat(this.#blinkCounter > 0 ? times : ''))
+  }
+
+  loop() {
+    const video = this.#camera.video
+    const img = this.#view.getVideoFrame(video)
+
+    this.#worker.send(img)
+
+    this.updateStatus(`Blinked ${this.#blinkCounter} times`)
+
+    setTimeout(() => this.loop(), 500)
   }
 
   onBtnClick() {
     this.updateStatus('Initializing...')
+    this.#blinkCounter = 0
+    this.loop()
   }
+
 }
