b95a19dcca16cc74dd69cbf50419726db22e1b22
Ivan Seibel <me@ivanseibel.dev>
2023-03-03 15:51:38 +0000

    get instance of btnInit and status elements

--- src/pages/video-player/src/view.js
+++ src/pages/video-player/src/view.js
@@ -1,4 +1,6 @@
 export default class View {
+  #btnInit = document.querySelector('#init')
+  #statusElement = document.querySelector('#status')
 
   enableButton() {
     this.#btnInit.disabled = false
